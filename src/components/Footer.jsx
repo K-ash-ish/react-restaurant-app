b59e572ef9e215a146768be0650de9ae5a464aa4
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-24 14:21:59 +0530

    Footer COmpleted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   // need to change button to link
   return (
     <footer className="border-t-2   py-4 w-full flex  justify-around items-center">
       <div className="social-links ">
-        <button href="#" className="px-2">
-          Github
-        </button>
-        <button href="#" className="px-2">
-          Linked
-        </button>
-        <button href="#" className="px-2">
-          Twitter
-        </button>
+        <a href="https://github.com/K-ash-ish" className="px-2">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kashish-sondhiya-969120198/"
+          className="px-2"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://twitter.com/KashishSo" className="px-2">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
       </div>
       <p>
         Made with ❤️ by <span className="underline font-semibold">Kashish</span>{" "}
