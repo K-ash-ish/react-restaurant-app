764bd17ede5fe595450d5a8c329a29171bb76574
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-24 13:46:55 +0530

    Adding active class to the navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="flex flex-col md:flex-row md:justify-around w-full items-center justify-around my-2 py-1 border-b-2 h-28">
+    <nav className="flex flex-col md:flex-row md:justify-around w-full items-center justify-around my-2 py-1 border-b-2 h-20">
       {" "}
       <h1 className="text-3xl tracking-wider font-bold">
         Eat<span className="text-rose-500 tracking-wider">Treat</span>
       </h1>
       <ul className="flex flex-row justify-around items-center text-lg  w-full md:w-1/3 h-14 ">
-        <Link to="/">
+        <NavLink to="/">
           <li className="nav-item">Home</li>
-        </Link>
-        <Link to="/cart">
+        </NavLink>
+        <NavLink to="/cart">
           <li className="nav-item ">
             Cart <span className="text-lg font-bold">{}</span>
           </li>
-        </Link>
-        <Link to="/login">
+        </NavLink>
+        <NavLink to="/login">
           <li className="nav-item ">Login</li>
-        </Link>
+        </NavLink>
       </ul>
     </nav>
   );
