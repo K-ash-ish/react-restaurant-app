7b18be3fd661d39aca3d1f08b8a13a2fb700e428
K-ash-ish <kashishsondhiya@gmail.com>
2023-02-24 13:38:35 +0530

    Navigation links working fine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Body from "./components/Body";
+import Cart from "./components/Cart";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Login from "./components/Login";
-
-function App() {
+function AppLayout() {
   return (
     <div className=" flex flex-col justify-between min-h-screen md:min-h-screen">
       <Header />
-      <Login />
-      {/* <Body /> */}
+      <Outlet />
       <Footer />
     </div>
   );
 }
+
+// const router = createBrowserRouter([
+//   {
+//     element: <AppLayout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Body />,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//     ],
+//   },
+// ]);
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element=<AppLayout />>
+        <Route path="/" element=<Body /> />
+        <Route path="login" element=<Login /> />
+        <Route path="cart" element=<Cart /> />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <nav className="flex flex-col md:flex-row md:justify-around w-full items-center justify-around my-2 py-1 border-b-2 h-28">
@@ -6,14 +8,17 @@ function Header() {
         Eat<span className="text-rose-500 tracking-wider">Treat</span>
       </h1>
       <ul className="flex flex-row justify-around items-center text-lg  w-full md:w-1/3 h-14 ">
-        {/* <Link></Link> */}
-        <li className="nav-item">Home</li>
-        {/* <Link></Link> */}
-        <li className="nav-item ">
-          Cart <span className="text-lg font-bold">{}</span>
-        </li>
-        {/* <Link></Link> */}
-        <li className="nav-item ">Login</li>
+        <Link to="/">
+          <li className="nav-item">Home</li>
+        </Link>
+        <Link to="/cart">
+          <li className="nav-item ">
+            Cart <span className="text-lg font-bold">{}</span>
+          </li>
+        </Link>
+        <Link to="/login">
+          <li className="nav-item ">Login</li>
+        </Link>
       </ul>
     </nav>
   );
