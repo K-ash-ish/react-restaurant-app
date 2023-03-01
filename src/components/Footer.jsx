24c112e7b870a5f83e3e16e90bfa838d765207b2
K-ash-ish <kashishsondhiya@gmail.com>
2023-03-01 19:33:44 +0530

    footer layout fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,22 +7,22 @@ import {
 function Footer() {
   // need to change button to link
   return (
-    <footer className="border-t-2   py-4 w-full flex  justify-around items-center">
-      <div className="social-links ">
-        <a href="https://github.com/K-ash-ish" className="px-2">
+    <footer className="border-t-2  py-4 w-full flex  justify-around items-center">
+      <div className="social-links text-4xl w-1/2 md:w-1/5 flex justify-around items-center">
+        <a href="https://github.com/K-ash-ish" className="">
           <FontAwesomeIcon icon={faGithub} />
         </a>
         <a
           href="https://www.linkedin.com/in/kashish-sondhiya-969120198/"
-          className="px-2"
+          className=""
         >
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
-        <a href="https://twitter.com/KashishSo" className="px-2">
+        <a href="https://twitter.com/KashishSo" className="">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
       </div>
-      <p>
+      <p className="w-1/2 text-center">
         Made with ❤️ by <span className="underline font-semibold">Kashish</span>{" "}
       </p>
     </footer>
