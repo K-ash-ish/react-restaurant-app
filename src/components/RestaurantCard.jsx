0ad4ff3cf7441e2f3c9c8f71006a4f2e79a2d157
K-ash-ish <kashishsondhiya@gmail.com>
2023-03-01 19:28:29 +0530

    fixing layout of restaurant card for small screens

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -9,15 +9,15 @@ function ReastaurantCard(props) {
     slaString,
   } = props;
   return (
-    <div className="card w-full  p-4 m-3  flex  sm:flex-col  items-center justify-around hover:border-2 cursor-pointer">
-      <div className=" restaurant-img sm:w-full  w-1/4 h-24 flex items-center">
+    <div className="card w-full  p-4 m-3  flex  md:flex-col  items-center justify-around hover:border-2 cursor-pointer">
+      <div className=" restaurant-img md:w-full  w-1/4 h-24 flex items-center">
         <img
-          className="rounded-xl sm:rounded-none "
+          className="rounded-xl md:rounded-none "
           src={IMG_CDN_URL + cloudinaryImageId}
           alt={name + "restaurant"}
         />
       </div>
-      <div className="card-details  sm:w-full w-3/5">
+      <div className="card-details  md:w-full w-3/5">
         <h1 className="font-bold my-1">{name}</h1>
         <div className="cuisines  text-xs flex flex-wrap">
           <p>{cuisines.join(", ")}</p>
