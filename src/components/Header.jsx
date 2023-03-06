95c4c5a282d5d9dc0ba992a73fbf6123a9742af2
K-ash-ish <kashishsondhiya@gmail.com>
2023-03-06 20:09:18 +0530

    Fixing navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,21 @@ function Header() {
   return (
     <nav className="flex flex-col md:flex-row md:justify-around w-full items-center justify-around my-2 py-1 md:border-b-2 h-20">
       {" "}
-      <h1 className="text-3xl tracking-wider font-bold my-4">
-        Eat<span className="text-rose-500 tracking-wider">Treat</span>
-      </h1>
+      <NavLink to="/">
+        <h1 className="text-3xl tracking-wider font-bold my-4">
+          Eat<span className="text-rose-500 tracking-wider">Treat</span>
+        </h1>
+      </NavLink>
       <ul className="flex flex-row justify-around items-center text-lg  w-full md:w-1/3 h-14 ">
-        <NavLink to="/">
-          <li className="nav-item">Home</li>
-        </NavLink>
+        <div className="search-box h-10 w-1/2   flex flex-row justify-center items-center">
+          <input
+            type="text"
+            className="border-2 px-2 py-1 h-8 w-5/6 text-sm focus:outline-gray-300"
+          />
+          <button className="border-2 border-red-300 hover:bg-red-500 hover:text-white hover:border-none transition-colors ease-in duration-300  text-base px-3 py-1 mx-2 ">
+            Search
+          </button>
+        </div>
         <NavLink to="/cart">
           <li className="nav-item ">
             Cart <span className="text-lg font-bold">{}</span>
