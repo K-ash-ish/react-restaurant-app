c744b075d992ea7f4bd25c380c2fa73d35c1c7a2
K-ash-ish <kashishsondhiya@gmail.com>
2023-03-09 22:45:35 +0530

    Refactoring code

--- src/constant.js
+++ src/constant.js
@@ -5259,9 +5259,11 @@ export const restaurantPage = {
   csrfToken: "uNu69JVdj5lz-u-7kO9c4YfATRHYVYgTaFyIh8-o",
 };
 export const FETCH_RESTAURANT =
+  // "https://eat-treat-server.onrender.com/api/restaurants";
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&page_type=DESKTOP_WEB_LISTING";
 export const FETCH_MENU =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=";
+  // "https://eat-treat-server.onrender.com/api/restaurant/menu?id=";
+  "https://www.swiggy.com/dapi/menu/v4/full?lat=23.2599333&lng=77.412615&menuId=";
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export default restaurantList;
--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -2,18 +2,11 @@ import { useState, useEffect } from "react";
 import { FETCH_MENU } from "../constant";
 function useRestaurant(id) {
   const [restaurant, setRestaurant] = useState(null);
-  // const [menu, setMenu] = useState();
   useEffect(() => {
     const getRestaurantDetail = async () => {
-      const data = await fetch(
-        // "https://eat-treat-server.onrender.com/api/restaurant/menu?id="
-        FETCH_MENU + id
-      );
-      // const data = await fetch(
-      //   "http://localhost:4000/api/restaurant/menu?id=" + id
-      // );
+      const data = await fetch(FETCH_MENU + id);
       const json = await data.json();
-      setRestaurant(json.data);
+      setRestaurant(json?.data);
     };
     getRestaurantDetail();
   }, [id]);
