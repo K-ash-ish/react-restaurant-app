67f8844299e2e945e0e6f1228cb7967ec6067d19
K-ash-ish <kashishsondhiya@gmail.com>
2023-05-31 23:25:32 +0530

    menu fix

--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -1,24 +1,24 @@
 import { useState, useEffect } from "react";
 import { FETCH_MENU } from "../constant";
-// import useCurrentLocation from "./useCurrentLocation";
+import useCurrentLocation from "./useCurrentLocation";
 function useRestaurant(id) {
   const [restaurant, setRestaurant] = useState(null);
-  // const { latitude, longitude } = useCurrentLocation();
+  const { latitude, longitude } = useCurrentLocation();
   useEffect(() => {
     const getRestaurantDetail = async () => {
-      // const data = await fetch(
-      //   `${FETCH_MENU}id=${id}&longitude=${79.97640129999999}&latitude=${23.1768293}`
-      // );
       const data = await fetch(
-        `${FETCH_MENU}&lat=${23.1967235}&lng=${79.9247217}&restaurantId=${id}&submitAction=ENTER`
+        `${FETCH_MENU}id=${id}&longitude=${longitude}&latitude=${latitude}`
       );
+      // const data = await fetch(
+      //   `${FETCH_MENU}&lat=${latitude}&lng=${longitude}&restaurantId=${id}&submitAction=ENTER`
+      // );
       const json = await data.json();
       setRestaurant(json?.data);
       // const restaurantdetails = restaurantPage;
       // setRestaurant(restaurantdetails.data);
     };
     getRestaurantDetail();
-  }, [id]);
+  }, [id, latitude, longitude]);
   return restaurant;
 }
 export default useRestaurant;
