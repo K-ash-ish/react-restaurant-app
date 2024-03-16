9594a7e65f6a8a47e0a83f2e723a80125a5c9c1b
K-ash-ish <kashishsondhiya@gmail.com>
2024-03-16 15:15:12 +0530

    fix: transition fix

--- src/components/ui/ModalWrapper.jsx
+++ src/components/ui/ModalWrapper.jsx
@@ -5,9 +5,7 @@ function ModalWrapper({ children, showModal, setShowModal }) {
   return (
     <div
       className={` z-10 fixed top-0 right-0 left-0 bottom-0  flex items-center justify-center transition-all duration-700  ${
-        showModal
-          ? "translate-y-0 bg-[#7772723a]"
-          : "translate-y-20 opacity-0 pointer-events-none  "
+        showModal ? " bg-[#7772723a]" : " opacity-0 pointer-events-none  "
       }`}
       onClick={() => setShowModal(false)}
     >
